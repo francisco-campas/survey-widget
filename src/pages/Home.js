64e7f74ed34834515d6acb8a797df7f9ad2e213a
atlir <dmiriy.spirin@gmail.com>
2020-05-04 00:23:55 +0300

    Add basic survey component

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SurveyComponent from './../components/SurveyComponent';
+
+const Home = (props) => (
+    <div>
+        <h1>Page with survey</h1>
+        <div>
+            <SurveyComponent eventId="F824Bb7abtQpvv07kjrX" surveyId="qpSogKlTVVQSTgJ5Aakb" userId="1wewd9asdasdnas9as"/>
+        </div>
+    </div>
+)
+
+export default Home;
